08a1a38ec813648f8de568c2f045c25d4d0303ba
Francesco Moscara <francesco.moscara@gmail.com>
2023-06-19 11:14:17 +0200

    gf

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include "mainwindow.h"
 #include <QApplication>
 
-int main(int argc, char *argv[])
-{
+// file:///home/moscara/Scrivania/WEBRTC/HTML/index.html
+
+int main(int argc, char *argv[]) {
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
